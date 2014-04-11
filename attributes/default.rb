6b8b16bdfcdf75827068758ba53b03c264239fd4
Enrico Mrass <github@emrass.de>
2014-04-11 22:03:05 +0200

    default attributes

---
+++ attributes/default.rb
@@ -0,0 +1,26 @@
+# fail2ban.conf configuration options
+default['fail2ban']['loglevel']   = 3
+# default['fail2ban']['socket']     = '/var/run/fail2ban/fail2ban.sock'
+# default['fail2ban']['logtarget']  = '/var/log/fail2ban.log'
+
+# jail.conf configuration options
+default['fail2ban']['ignoreip']   = '127.0.0.1/8'
+default['fail2ban']['bantime']    = 300
+default['fail2ban']['maxretry']   = 5
+default['fail2ban']['backend']    = 'polling'
+default['fail2ban']['email']      = 'root@localhost'
+default['fail2ban']['action']     = 'action_'
+default['fail2ban']['banaction']  = 'iptables-multiport'
+default['fail2ban']['mta']        = 'sendmail'
+default['fail2ban']['protocol']   = 'tcp'
+default['fail2ban']['chain']      = 'INPUT'
+
+default['fail2ban']['services']   = {
+  'ssh' => {
+        'enabled' => 'true',
+        'port' => 'ssh',
+        'filter' => 'sshd',
+        'logpath' => node['fail2ban']['auth_log'],
+        'maxretry' => '6'
+     }
+}
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'enrico.mrass@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures tippfuchs-fail2ban'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'fail2ban'
 
