d79d0afe37961ac7400486376510105da892e5dc
Enrico Mrass <github@emrass.de>
2014-04-14 21:04:51 +0200

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,6 @@ maintainer_email 'enrico.mrass@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures tippfuchs-fail2ban'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 depends 'fail2ban'
-
